7a6b1e94becb7d6daf5e7d4013ab85b5e96c96ef
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2016-02-01 12:00:50 +0100

    Update dijkstrashortreach.java

--- dijkstrashortreach.java
+++ dijkstrashortreach.java
@@ -1,37 +1,36 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
-    
-    public class Pair<L,R> {
-      private final L left;
-      private final R right;
+class Pair<L,R> {
+    private final L left;
+    private final R right;
 
-      public Pair(L left, R right) {
-        this.left = left;
-        this.right = right;
-      }
+    public Pair(L left, R right) {
+      this.left = left;
+      this.right = right;
+    }
 
-      public L getLeft() { return left; }
-      public R getRight() { return right; }
+    public L getLeft() { return left; }
+    public R getRight() { return right; }
 
-      @Override
-      public int hashCode() {
-          return left.hashCode() ^ right.hashCode();
-         // long l = left.hashCode() * 2654435761L; 
-          //return (int)l + (int)(l >>> 32) + right.hashCode();
-      }
+    @Override
+    public int hashCode() {
+       return left.hashCode() ^ right.hashCode();
+    }
 
-      @Override
-      public boolean equals(Object o) {
-        if (!(o instanceof Pair)) return false;
+    @Override
+    public boolean equals(Object o) {
+      if (!(o instanceof Pair)) return false;
         Pair pairo = (Pair) o;
         return this.left.equals(pairo.getLeft()) &&
                this.right.equals(pairo.getRight());
-      }
-
     }
+
+}
+
+public class Solution {
+    
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Map<Tuple, Set<Pair<Character,Integer>>> adj = new HashMap<Tuple, Set<Pair<Character,Integer>>>();
     }
 }
