aea9b7912acf20d3129fb36a4977c94da3a16ffb
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2016-02-03 07:06:23 +0100

    Update ReverseShuffleMerge.java

--- ReverseShuffleMerge.java
+++ ReverseShuffleMerge.java
@@ -11,13 +11,24 @@ public class Solution {
         while (it.hasNext()) {
             Map.Entry pair = (Map.Entry)it.next();
             System.out.println(pair.getKey() + " = " + pair.getValue());
-            //it.remove(); // avoids a ConcurrentModificationException
         }
+    }
+    
+    public static void printCount(String str) {
+        int[] ct = new int[26];
+        for(int i=0; i<str.length();i++){
+            char c = str.charAt(i);
+            ct[c-'a'] += 1;
+        }
+        System.out.println(Arrays.toString(ct) );
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
+        //System.out.format("string length: %s\n", str.length());
+        //in.nextLine();
+        //String actAns = in.nextLine();
         int[] ct = new int[26];
         int[] ori = new int[26];
         Map<Character, Set<Integer>> loc = new HashMap<Character, Set<Integer>>();
@@ -36,6 +47,7 @@ public class Solution {
             ori[i] = ct[i]/2;
         }
         //System.out.println(Arrays.toString(ori) );
+        //printCount(actAns);
         PriorityQueue<Character> q = new PriorityQueue<>();
         for(int i=0; i<26;i++){
             if(ori[i] > 0) {
@@ -45,34 +57,51 @@ public class Solution {
         }
         List<Character> ans = new ArrayList<>();
         int oriLen = str.length()/2;
+        StringBuilder sb = new StringBuilder(oriLen);
         for(int i=str.length()-1; i>=0;i--){
             char c = str.charAt(i);
             if (ori[c -'a'] == 0) continue;
             if (oriLen == 0) break;
-            //if (loc.get(c) == null) System.out.println( "null: " + Integer.toString(i));
             // if current smallest use it
-            int size = loc.get(c).size();
+            //int size = loc.get(c).size();
+            boolean toAdd = false;
             if(c == q.peek()) {
-                q.remove(c);
-                ans.add(c);
-                oriLen--;
-                ori[c -'a']--;
-                loc.get(c).remove(i);
+                toAdd = true;
+                //loc.get(c).remove(i);
             }                 
             // we cannot neglect this
-            else if(size == ori[c -'a']) {
+            else if(ct[c -'a'] == ori[c -'a']) {
+                toAdd = true;
+                //loc.get(c).remove(i);
+            }
+            // select a character "c" if next critical character turns out to be larger than "c". 
+            else if(ct[c -'a'] > ori[c -'a']) {
+                // look ahead in the string
+                for(int j = i-1; j>=0; j--){
+                    char cj = str.charAt(j);
+                    if (ori[cj -'a'] == 0) continue;
+                    if( (((int)cj) < ((int)c)) ) { break; } // further ahead there is a lesser(selectable) character so no need to select this
+                    if( (ori[cj -'a'] == ct[cj -'a']) ) {
+                        if(((int)cj) > ((int)c) ) {
+                            toAdd = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            if(toAdd) {
                 q.remove(c);
-                ans.add(c);
+                sb.append(c);
                 oriLen--;
                 ori[c -'a']--;
-                loc.get(c).remove(i);
             }
-            // we can neglect
-            else if(size > ori[c -'a']) {
-                loc.get(c).remove(i);
-            }
+            ct[c -'a']--;
         }
+        System.out.println(sb.toString());
+        //printCount(sb.toString());
+        /*
         for(char c: ans)
             System.out.print(c);
+            */
     }
 }
